30dd9946165462b4b2f0a1473d17754f407586b2
skornbluh <skornbluh@gmail.com>
2017-06-12 15:54:03 -0500

    Update project04_queries.sql

--- project04_queries.sql
+++ project04_queries.sql
@@ -1,59 +1,81 @@
 #
 # A query to retrieve the major fields for every person
 #
-#select id,person_type,sec_id,last_name,first_name,street,city,state,zipcode,country,email_address1 from Persons;
+	select * from Persons;
 
 #
 # A query to retrieve the email(s) of a given person
 #
-#select email_address1,email_address2,email_address3,email_address4,email_address5 from Persons where id = '231';
+	select * from Emails where person_id = '944c';
 
 #
 # A query to add an email to a specific person
 #
+	INSERT INTO Emails (person_id, email_address) VALUES ('123lst','skornbluh@gmail.com');
+	select * from Emails where person_id = '123lst';
 
 #
 # A query to change the email address of a given email record
 #
 
+	UPDATE Emails
+	SET email_address = 'tinyrick@pleasehelpme.com'
+	WHERE person_id = '944c' and id = 1;
+
+	select * from Emails where id = 1;
+
 #
 # A query (or series of queries) to remove a given person record
 #
-#delete persons, portfolios from persons, portfolios where
-#(persons.id = portfolios.owner_id or
-#persons.id = portfolios.broker_id or
-#persons.id = portfolios.beneficiary_id) and
-#persons.id = '6doc';
-
+#SELECT * FROM Portfolios;
+#DELETE * FROM Portfolios where 
 # A query to create a person record
 #INSERT INTO Persons VALUES ('1001d','C', NULL, 'Kornbluh', 'Sarah','1226 P Street','Lincoln','NE','68508','USA', 'skornbluh@gmail.com', NULL, NULL, NULL, NULL);
 
 #
 # A query to get all the assets in a particular portfolio
 #
-#select 
-#	assets.id, assets.account_type, assets.label, assets.apr, assets.total_balance, assets.base_rate_of_return, assets.quarterly_dividend, assets.omega_measure, assets.percentage
-#	from assets, portfolios 
-  #  where assets.id = portfolios.account_id and portfolios.id = 'PF002';
-  
+	select * from Portfolios, Assets where Portfolios.id = 'PF200' and Assets.id = Portfolios.account_id;
+#
 # A query to get all the assets of a particular person
+#
+	select * from Portfolios, Assets where Assets.id = Portfolios.account_id  and Portfolios.owner_id = '2ndbestd';
 
+#
 # A query to create a new asset record
-#INSERT INTO Assets VALUES('SVEMNY','D','Savings Account','0.5#', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+#
+	INSERT INTO Assets VALUES('SVEMNY','D','Savings Account','0.5', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 # A query to create a new portfolio record
-INSERT INTO Portfolios VALUES('SK3497','kdoame','394','4slbd','495KK');
+	INSERT INTO Portfolios VALUES('SK3497','kdoame','394','4slbd','495KK');
 
 # A query to associate a particular asset with a particular portfolio
-
+	UPDATE Portfolios
+	SET account_id = 'IND321'
+	WHERE Portfolios.id = 'PF200';
+	select * from Portfolios where account_id = 'IND321';
+    
+#
 # A query to find the total number of portfolios owned by each person
+#
+	SELECT owner_id, COUNT(*) FROM Portfolios GROUP BY owner_id;
 
+#
 # A query to find the total number of portfolios managed by each person
+#
+	SELECT broker_id, COUNT(*) FROM Portfolios GROUP BY broker_id;
 
+	select id,account_type,stock_symbol, share_price, number_of_shares,total_value from Assets;
+    
+#
 # A query to find the total value of all stocks in each portfolio (hint: you can take an
 # aggregate of a mathematical expression)
+
+	SELECT sum(total_value)
+	FROM Portfolios,Assets where Assets.account_type = 'S' group by Portfolios.id;
 
 # A query to detect an invalid distribution of private investment assets (that is, a query
 # to add up the stake percentage of each such asset and return a list of investments
 # whose percentage exceeds 100%)
 #
+	select id,account_type, percentage from Assets GROUP BY id; 
