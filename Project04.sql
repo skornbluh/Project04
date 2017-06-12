385c10ec37eda16e3fb380077208b675ecebcc50
skornbluh <skornbluh@gmail.com>
2017-06-12 16:42:54 -0500

    Update Project04.sql

--- Project04.sql
+++ Project04.sql
@@ -13,33 +13,67 @@ CREATE TABLE Persons (
     state varchar(30) not null,
     zipcode varchar(12),
     country varchar(40) not null,
-    email_address1 varchar(60),
-	email_address2 varchar(60),
-	email_address3 varchar(60),
-    email_address4 varchar(60),
-	email_address5 varchar(60),
     PRIMARY KEY (id)
 );
 
-INSERT INTO Persons VALUES ('944c','C', NULL, 'Castro', 'Starlin','1060 West Addison Street','Chicago','IL','60613','USA', 'scastro@cubs.com','starlin_castro13@gmail.com', NULL, NULL, NULL);
-INSERT INTO Persons VALUES ('306a','C', NULL, 'Sampson', 'Brock','123 N 1st Street','Omaha','NE','68116','USA','brock_f_sampson@gmail.com','bsampson@venture.com', NULL, NULL, NULL);
-INSERT INTO Persons VALUES ('2342','C', NULL, 'O\'Brien','Miles','123 Friendly Street','Ottawa','ON','K1A 0G9','Canada', NULL, NULL, NULL, NULL, NULL);
-INSERT INTO Persons VALUES ('aef1','E','sec001','Gekko', 'Gordon','1 Wall Street','New York','NY','10005-0012','USA', NULL, NULL, NULL, NULL, NULL);
-INSERT INTO Persons VALUES ('321f','J','sec991','Fox', 'Bud','321 Bronx Street','New York','NY','10004','USA','bfox@gmail.com','csheen@crazy.net', NULL, NULL, NULL);
-INSERT INTO Persons VALUES ('ma12', 'C', NULL, 'Sveum', 'Dale', '1060 West Addison Street', 'Chicago', 'IL', '60613', 'USA', 'sveum@cubs.com', NULL, NULL, NULL, NULL);
-INSERT INTO Persons VALUES ('321nd','C', NULL, 'Hartnell', 'William', '1060 West Addison Street', 'Chicago','IL','60613','USA','whartnell@doctors.com','dr@who.com', NULL, NULL, NULL);
-INSERT INTO Persons VALUES ('nf32a','C',NULL, 'Troughton', 'Patrick','1060 West Addison Street','Chicago','IL','60613','USA','ptroug@cse.unl.edu','ptrou32@unl.edu', NULL, NULL, NULL);
-INSERT INTO Persons VALUES ('321na','E','sec125','Pertwee', 'Jon','301 Front St W', 'Toronto', 'ON', 'M5V 2T6', 'Canada','jpet@whofan.com', NULL, NULL, NULL, NULL);
-INSERT INTO Persons VALUES ('231','E','sec221','Baker','Tom','1 Blue Jays Way', 'Toronto', 'ON', 'M5V 1J1', 'Canada','famousdoc@who.com','tbaker@cse.unl.edu','mostfamous@whovian.com','thedoctor@bbc.com', NULL);
-INSERT INTO Persons VALUES ('6doc','J','sec982','Hurndall', 'Richard','Campos El290','Mexico City', 'FD', NULL, 'Mexico','rhurndall@cse.unl.edu','richard@unl.edu', NULL, NULL, NULL);
-INSERT INTO Persons VALUES ('321dr','J','sec543','Baker', 'C.','Avery Hall','Lincoln','NE','68503','USA','dr@baker.com', NULL, NULL, NULL, NULL);
-INSERT INTO Persons VALUES ('1svndr','C',NULL, 'McCoy','Sylvester','126-01 Roosevelt Ave', 'Flushing', 'NY','11368','USA','slyguy@hotmail.com','mccoy@whofan.com', NULL, NULL, NULL);
-INSERT INTO Persons VALUES ('123lst','E','sec31x','McGann', 'Paul','1 MetLife Stadium Dr', 'East Rutherford', 'NJ','07073','USA','pmcgann@mlb.com','foo@bar.com','pmc@unl.edu', NULL, NULL);
-INSERT INTO Persons VALUES ('nwdoc1','J','sec953','Eccleston', 'Chris','1 E 161st', 'St, Bronx', 'NY','10451','USA','newguy@whovian.com', NULL, NULL, NULL, NULL);
-INSERT INTO Persons VALUES ('2ndbestd','C',NULL, 'Tennant', 'David','700 E Grand Ave', 'Chicago', 'IL', '60611','USA','actor@shakespeare.com','tdavid@unl.edu', NULL, NULL, NULL);
-INSERT INTO Persons VALUES ('wrddoc','C',NULL, 'Smith', 'Matt','333 W 35th St,', 'Chicago', 'IL','60616','USA','msmith@who.com','thedoc@cse.unl.edu', NULL, NULL, NULL);
-INSERT INTO Persons VALUES ('bbchar','C',NULL, 'Ehrmantraut', 'Kaylee','800 West 7th Street', 'Albuquerque', 'NM', '87105','USA', NULL, NULL, NULL, NULL, NULL);
-INSERT INTO Persons VALUES ('doc05','C',NULL, 'Davison', 'Peter','123 Cabo San Lucas', 'Los Cabos', 'BCS', NULL, 'Mexico', NULL, NULL, NULL, NULL, NULL);
+INSERT INTO Persons VALUES ('944c','C', NULL, 'Castro', 'Starlin','1060 West Addison Street','Chicago','IL','60613','USA');
+INSERT INTO Persons VALUES ('306a','C', NULL, 'Sampson', 'Brock','123 N 1st Street','Omaha','NE','68116','USA');
+INSERT INTO Persons VALUES ('2342','C', NULL, 'O\'Brien','Miles','123 Friendly Street','Ottawa','ON','K1A 0G9','Canada');
+INSERT INTO Persons VALUES ('aef1','E','sec001','Gekko', 'Gordon','1 Wall Street','New York','NY','10005-0012','USA');
+INSERT INTO Persons VALUES ('321f','J','sec991','Fox', 'Bud','321 Bronx Street','New York','NY','10004','USA');
+INSERT INTO Persons VALUES ('ma12', 'C', NULL, 'Sveum', 'Dale', '1060 West Addison Street', 'Chicago', 'IL', '60613', 'USA');
+INSERT INTO Persons VALUES ('321nd','C', NULL, 'Hartnell', 'William', '1060 West Addison Street', 'Chicago','IL','60613','USA');
+INSERT INTO Persons VALUES ('nf32a','C',NULL, 'Troughton', 'Patrick','1060 West Addison Street','Chicago','IL','60613','USA');
+INSERT INTO Persons VALUES ('321na','E','sec125','Pertwee', 'Jon','301 Front St W', 'Toronto', 'ON', 'M5V 2T6', 'Canada');
+INSERT INTO Persons VALUES ('231','E','sec221','Baker','Tom','1 Blue Jays Way', 'Toronto', 'ON', 'M5V 1J1', 'Canada');
+INSERT INTO Persons VALUES ('6doc','J','sec982','Hurndall', 'Richard','Campos El290','Mexico City', 'FD', NULL, 'Mexico');
+INSERT INTO Persons VALUES ('321dr','J','sec543','Baker', 'C.','Avery Hall','Lincoln','NE','68503','USA');
+INSERT INTO Persons VALUES ('1svndr','C',NULL, 'McCoy','Sylvester','126-01 Roosevelt Ave', 'Flushing', 'NY','11368','USA');
+INSERT INTO Persons VALUES ('123lst','E','sec31x','McGann', 'Paul','1 MetLife Stadium Dr', 'East Rutherford', 'NJ','07073','USA');
+INSERT INTO Persons VALUES ('nwdoc1','J','sec953','Eccleston', 'Chris','1 E 161st', 'St, Bronx', 'NY','10451','USA');
+INSERT INTO Persons VALUES ('2ndbestd','C',NULL, 'Tennant', 'David','700 E Grand Ave', 'Chicago', 'IL', '60611','USA');
+INSERT INTO Persons VALUES ('wrddoc','C',NULL, 'Smith', 'Matt','333 W 35th St,', 'Chicago', 'IL','60616','USA');
+INSERT INTO Persons VALUES ('bbchar','C',NULL, 'Ehrmantraut', 'Kaylee','800 West 7th Street', 'Albuquerque', 'NM', '87105','USA');
+INSERT INTO Persons VALUES ('doc05','C',NULL, 'Davison', 'Peter','123 Cabo San Lucas', 'Los Cabos', 'BCS', NULL, 'Mexico');
+
+CREATE TABLE Emails (
+	id varchar(10) primary key not null,
+    person_id char(10) not null,
+    email_address varchar(60),
+	CONSTRAINT FK_EmailPersons FOREIGN KEY (person_id) REFERENCES Persons(id)
+    ON DELETE CASCADE
+);
+
+INSERT INTO Emails VALUES ('1','944c','scastro@cubs.com');
+INSERT INTO Emails VALUES('2','944c','starlin_castro13@gmail.com');
+INSERT INTO Emails VALUES('3','306a','brock_f_sampson@gmail.com');
+INSERT INTO Emails VALUES('4','306a','bsampson@venture.com');
+INSERT INTO Emails VALUES('5','321f','bfox@gmail.com');
+INSERT INTO Emails VALUES('6','321f','csheen@crazy.net');
+INSERT INTO Emails VALUES('7','ma12','sveum@cubs.com');
+INSERT INTO Emails VALUES('8','321nd','whartnell@doctors.com');
+INSERT INTO Emails VALUES('9','321nd','dr@who.com');
+INSERT INTO Emails VALUES('10','nf32a','ptroug@cse.unl.edu');
+INSERT INTO Emails VALUES('11','nf32a','ptrou32@unl.edu');
+INSERT INTO Emails VALUES('12','321na','jpet@whofan.com');
+INSERT INTO Emails VALUES('13','231','famousdoc@who.com');
+INSERT INTO Emails VALUES('14','231','tbaker@cse.unl.edu');
+INSERT INTO Emails VALUES('15','231','mostfamous@whovian.com');
+INSERT INTO Emails VALUES('16','231','thedoctor@bbc.com');
+INSERT INTO Emails VALUES('17','6doc','rhurndall@cse.unl.edu');
+INSERT INTO Emails VALUES('18','6doc','richard@unl.edu');
+INSERT INTO Emails VALUES('19','321dr','dr@baker.com');
+INSERT INTO Emails VALUES('20','1svndr','slyguy@hotmail.com');
+INSERT INTO Emails VALUES('21','1svndr','mccoy@whofan.com');
+INSERT INTO Emails VALUES('22','123lst','pmcgann@mlb.com');
+INSERT INTO Emails VALUES('23','123lst','foo@bar.com');
+INSERT INTO Emails VALUES('24','123lst', 'pmc@unl.edu');
+INSERT INTO Emails VALUES('25','nwdoc1','newguy@whovian.com');
+INSERT INTO Emails VALUES('26','2ndbestd','actor@shakespeare.com');
+INSERT INTO Emails VALUES('27','2ndbestd','tdavid@unl.edu');
+INSERT INTO Emails VALUES('28','wrddoc','msmith@who.com');
+INSERT INTO Emails VALUES('29','wrddoc','thedoc@cse.unl.edu');
+
 
 CREATE TABLE Assets (
 	id varchar(10),
@@ -82,16 +116,16 @@ INSERT INTO Assets VALUES('IND321','P','Industrial Widget Manufacturers Inc.',NU
 INSERT INTO Assets VALUES('RSTOR322','P','7" Vinyl Records',NULL,'2453.21',NULL,'2.1','0.54',NULL,'13500.00',NULL, NULL, NULL, NULL);
 
 CREATE TABLE Portfolios (
-	id varchar(10) not null,
+	id varchar(10) primary key not null,
     owner_id varchar(10)  not null,
     broker_id varchar(10) not null,
     beneficiary_id varchar(10),
     account_id varchar(10),
-    PRIMARY KEY (id),
    CONSTRAINT FK_PersonsOwner FOREIGN KEY (owner_id) REFERENCES Persons(id),
    CONSTRAINT FK_PersonsBroker FOREIGN KEY (broker_id) REFERENCES Persons(id),
    CONSTRAINT FK_PersonsBeneficiary FOREIGN KEY (beneficiary_id) REFERENCES Persons(id),
    CONSTRAINT FK_AssetsAccount FOREIGN KEY (account_id) REFERENCES Assets(id)
+   ON DELETE CASCADE
 );
 
 INSERT INTO Portfolios VALUES('PT001','944c','aef1','ma12','BX001-23');
@@ -104,3 +138,5 @@ INSERT INTO Portfolios VALUES('PF006','1svndr','231',NULL, NULL);
 INSERT INTO Portfolios VALUES('PF007','231','231','bbchar','321CC');
 INSERT INTO Portfolios VALUES('PF200','6doc','nwdoc1','bbchar', NULL);
 INSERT INTO Portfolios VALUES('PF300','1svndr','6doc',NULL,'IND321');
+
+
